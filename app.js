74ec19f4c7520fee6cec14cc300d7c33eaf28cd7
Alberto Ortiz <baortiz98@outlook.com>
2022-08-29 13:11:53 -0500

    Add functionalities of: crear, filtrar

--- app.js
+++ app.js
@@ -1,20 +1,31 @@
-const readTareas = require("./funcionesDeTareas");
-const NOTES = readTareas.leer();
+const tareas = require("./funcionesDeTareas");
+let notes = tareas.leer();
 
 let action = process.argv[2];
-
+const saltoLinea = "\n" + "-----------------------------------";
 switch(action) {
    case "listar":
-      console.log("Listado de tareas:" + "\n" + "-----------------------------------");
-      NOTES.forEach( (item, indice) => {
+      console.log("Listado de tareas:" + saltoLinea);
+      notes.forEach( (item, indice) => {
          console.log((indice+1) + ". " + item.titulo + " - " + item.estado)
       });
       break;
    case undefined:
-      console.log("No entiendo qué quieres hacer" +"\n" + "Las acciones disponibles son: Listar");
+      console.log("No entiendo qué quieres hacer" +"\n" + "Las acciones disponibles son: listar, crear, filtrar");
+      break;
+   case "crear":
+      let instruccion = process.argv[3];
+      tareas.guardarTarea({titulo: instruccion, estado: "pendiente"})
+      console.log("Tarea agregada exitosamente" + saltoLinea);
+      console.log(instruccion + ": pendiente");
+      notes = tareas.leer();
+      break;
+   case "filtrar":
+      console.log("La lista de tareas " + process.argv[3] + " es:" + saltoLinea );
+      tareas.filtrarPorEstado(process.argv[3]).forEach( e => console.log(e.titulo));
       break;
    default:
-      console.log("Tienes que pasar una acción" +"\n" + "Las acciones disponibles son: Listar");
+      console.log("Tienes que pasar una acción" + saltoLinea + "Las acciones disponibles son: listar, crear, filtrar");
    }
 
 // Otra forma em lugar del foreach:
--- funcionesDeTareas.js
+++ funcionesDeTareas.js
@@ -1,10 +1,23 @@
 const FS = require("fs");
 
-let readTareas = {
+let tareas = {
    archivo: "tareas.json",
    leer: function() {
       return JSON.parse(FS.readFileSync(this.archivo, { encoding: "utf-8"}));
+   },
+   escribirJSON: function(tasks) {
+      const text = JSON.stringify(tasks);
+      FS.writeFileSync(this.archivo,text);
+   },
+   guardarTarea: function(tasks) {
+      let matrixTareas = this.leer();
+      matrixTareas.push(tasks);
+      this.escribirJSON(matrixTareas);
+   },
+   filtrarPorEstado: function(estado) {
+      let text = this.leer();
+      return text.filter( e => e.estado == estado);
    }
 }
 
-module.exports = readTareas;
+module.exports = tareas;
